ec64f83753d51c1ac3a89759aa3d15f15bc23f7c
Alfonso E.M <alfonso@el-magnifico.org>
2017-03-18 20:30:07 +0000

    Main page shows hostname and pclabel

--- flask-version/unamano.py
+++ flask-version/unamano.py
@@ -1,8 +1,10 @@
 
-# A very simple Flask Hello World app for you to get started with...
+# UNAMANO
+# Remote support
 
 from flask import Flask, request, render_template
 import os
+import datetime
 
 app = Flask(__name__)
 
@@ -12,7 +14,12 @@ sessions={}
 
 @app.route('/')
 def index():
-    current_sessions=os.listdir(base_dir)
+    dirs=os.listdir(base_dir)
+    current_sessions={}
+    for d in dirs:
+        with open(base_dir+"/"+d+"/id") as f:
+            session_id=f.readline()
+        current_sessions[d]=session_id
     return render_template('sessions.html', sessions=current_sessions)
 
 @app.route('/session/<uid>')
@@ -21,31 +28,57 @@ def session(uid='NO SESSION UID'):
     return render_template('commands.html', uid=uid, commands=commands)
 
 @app.route('/output/<uid>/<cid>')
-def output(uid='NO SESSION UID',cid='0'):
+def output(uid='NO SESSION UID',cid=''):
     logdir=base_dir+"/"+uid
     logfile=logdir+"/"+cid
     with open(logfile,"r") as f:
         output=f.read()
     return render_template('output.html', uid=uid, cid=cid, output=output)
 
-@app.route('/cmd', methods=['POST','GET'])
-def cmd():
+@app.route('/command',methods=['POST'])
+def command():
+    uid=request.form['uid']
+    logdir=base_dir+"/"+uid
+    commandfile=logdir+"/command"
+    command=request.form['command']
+
+    with open(commandfile,"w") as f:
+        f.write(command)
+    return render_template('commands.html', uid=uid, commands="")
+
+
+
+@app.route('/client', methods=['POST','GET'])
+def client():
+    hostname=request.headers['hostname']
+    pclabel=request.headers['pclabel']
     uid=request.headers['sessionid']
+
+    logdir=base_dir+"/"+uid
+
+    if not os.path.exists(logdir):
+        os.makedirs(logdir)
+        timestamp=datetime.datetime.now()
+        with open(logdir+"/id","w") as f:
+             f.write(timestamp.isoformat()+" "+hostname+"-"+pclabel)
+
     if request.method == 'GET':
-        command="ls"
+        commandfile=logdir+"/command"
+        try:
+           with open(commandfile,"r") as f:
+                command=f.read()
+        except:
+            command=""
         if uid not in sessions:
             sessions[uid]=0
         return command
+
     if request.method == 'POST':
-        logdir=base_dir+"/"+uid
         logfile=logdir+"/"+str(sessions[uid])
-        if not os.path.exists(logdir):
-            os.makedirs(logdir)
-        with open(logfile,"w") as f:
-            f.write(request.form['output'])
+        file = request.files['output']
+        file.save(logfile)
+
         sessions[uid]+=1
         return "OK"
-
-
 
 
